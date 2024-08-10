04d492322e28d41e5d0df4b9b77260f94b89af45
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-08-09 23:04:38 -0300

    substituir a URL base para o endpoint de produção

--- meu-frontend/src/services/api.js
+++ meu-frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:5000"
-})
+    baseURL: "https://user-management-app-three.vercel.app/"
+});
 
 api.interceptors.request.use(
     (config) => {
